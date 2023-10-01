2310c14ff3e8d416b0eecdc681afb3575eb8982e
cesar <cesarpazol1029@gmail.com>
2023-09-30 23:50:41 -0500

    Manual authentication.

--- src/main/java/com/cesar/Manual_Authentication/controller/BasicController.java
+++ src/main/java/com/cesar/Manual_Authentication/controller/BasicController.java
@@ -1,22 +1,62 @@
 package com.cesar.Manual_Authentication.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import jakarta.servlet.http.HttpSession;
+
+@RestController("/")
 public class BasicController {
 	
 	
-	@GetMapping("/authenticate")
-	String authenticate(){
+	
+	@RequestMapping("login")
+	String login() {
 		
-		return "Login system will be here. It's public to everyone!";
+		return "Not authenticated. This sumulates the redirect to the login page.";
 	}
 	
 	
-	@GetMapping("/access")
+	
+	@RequestMapping("authenticate/{name}/{password}")
+	void authenticate(@PathVariable String name, @PathVariable String password, HttpSession session){
+			
+		UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(name, password);
+	
+		Authentication authentication = authManager.authenticate( token );	
+		
+		SecurityContext sc = SecurityContextHolder.getContext();
+		
+		sc.setAuthentication( authentication );
+
+		session.setAttribute(HttpSessionSecurityContextRepository.SPRING_SECURITY_CONTEXT_KEY, sc);		
+	}
+	
+	
+	
+	
+	@RequestMapping("access")
 	String authenticatedAccess(){
 		
 		return "You're authenticated!";
 	}
+	
+	
+	
+	
+	
+	@Autowired
+	AuthenticationManager authManager;
+	
+	@Autowired
+	UserDetailsService uds;
 }
--- src/main/java/com/cesar/Manual_Authentication/security/SecurityConfig.java
+++ src/main/java/com/cesar/Manual_Authentication/security/SecurityConfig.java
@@ -5,9 +5,13 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
 
 
 @Configuration
@@ -16,17 +20,23 @@ public class SecurityConfig {
 	@Bean
 	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		
-			return http.
-					
-				csrf( csrf -> csrf
+			return http
+				
+				.csrf( csrf -> csrf
 					.disable() )
 				
-				.authorizeHttpRequests( requests -> requests
-					.requestMatchers("/authenticate").permitAll()
-					.anyRequest().authenticated() )
+				.exceptionHandling( exHandling -> exHandling
+						.authenticationEntryPoint( new LoginUrlAuthenticationEntryPoint("/login")) )
 				
+				.authorizeHttpRequests( requests -> requests
+					.requestMatchers("/access").authenticated()
+					.anyRequest().permitAll() ) 
+					
 			.build();
 	}
+	
+
+	
 	
 	
 	@Bean
@@ -40,9 +50,17 @@ public class SecurityConfig {
 	@Bean
 	UserDetailsService userDetailsService() {
 		
-		return new InMemoryUserDetailsManager();
+		var userDetails = new InMemoryUserDetailsManager();
+		
+		userDetails.createUser( User.withUsername("cesar").password( passwordEncoder().encode("123") ).build() );
+		
+		return userDetails;		
 	}
 	
 	
-
+	@Bean
+	PasswordEncoder passwordEncoder() {
+		
+		return new BCryptPasswordEncoder();
+	}
 }
